287d8656f0f2562828ae89298a3ba85f86f38df8
gjones81 <jones.guy81@gmail.com>
2020-05-01 15:29:31 -0700

    Initial commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const methodOverride = require('method-override');
+let db = require('./models')
 
 const app = express();
 
@@ -9,7 +10,33 @@ app.use(express.static('static'));
 app.use(methodOverride('_method'));
 
 // WRITE YOUR ROUTES HERE /////////////////////
+app.get('/', (req, res) => {
+	db.widget.findAll()
+	.then((widget) => {
+		res.render('index', {widget})
+	})
+	
+})
 
+app.post('/', (req, res) => {
+	db.widget.create(req.body)
+	.then(() => {
+		res.redirect('/')
+	})
+})
+
+app.delete('/delete', (req, res) => {
+	db.widget.destroy({
+		where: {
+			name: req.body.name,
+			quantity: req.body.quantity
+
+		}
+	})
+	.then(() => {
+		res.redirect('/')
+	})
+})
 
 
 // YOUR ROUTES ABOVE THIS COMMENT /////////////
